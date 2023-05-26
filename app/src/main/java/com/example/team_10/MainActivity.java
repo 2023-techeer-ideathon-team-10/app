0d4e8bcbf84c0b6f1b6a4fefae6ba65b431917d9
jujeong <jujeong0108@naver.com>
2023-05-27 03:05:20 +0900

    Divided project in two bottom navigation bar Qr Scanning, Seat Display

--- app/src/main/java/com/example/team_10/MainActivity.java
+++ app/src/main/java/com/example/team_10/MainActivity.java
@@ -1,123 +1,60 @@
 package com.example.team_10;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
-
-import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.util.Log;
-import android.util.SparseArray;
-import android.view.SurfaceHolder;
-import android.view.SurfaceView;
+import android.view.MenuItem;
 import android.widget.Toast;
 
-import android.Manifest;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
-import com.google.android.gms.vision.CameraSource;
-import com.google.android.gms.vision.Detector;
-import com.google.android.gms.vision.barcode.Barcode;
-import com.google.android.gms.vision.barcode.BarcodeDetector;
+import com.example.team_10.qrscanner.QRFragment;
+import com.example.team_10.seat.SeatFragment;
 
-import java.io.IOException;
+import com.example.team_10.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final String TAG = "QRCodeScanner";
-    private static final int CAMERA_PERMISSION_REQUEST_CODE = 200;
-
-    private SurfaceView surfaceView;
-    private CameraSource cameraSource;
+    private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        surfaceView = findViewById(R.id.surfaceView);
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setOnNavigationItemSelectedListener(navItemSelectedListener);
 
-        // Create a barcode detector
-        BarcodeDetector barcodeDetector = new BarcodeDetector.Builder(this)
-                .setBarcodeFormats(Barcode.QR_CODE)
-                .build();
+        // Set QRFragment as the default fragment
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fragmentContainer, new QRFragment())
+                .commit();
+    }
 
-        // Create a camera source
-        cameraSource = new CameraSource.Builder(this, barcodeDetector)
-                .setRequestedPreviewSize(640, 480)
-                .setAutoFocusEnabled(true)
-                .build();
+    private BottomNavigationView.OnNavigationItemSelectedListener navItemSelectedListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selectedFragment = null;
 
-        // Add surface view callback
-        surfaceView.getHolder().addCallback(new SurfaceHolder.Callback() {
-            @Override
-            public void surfaceCreated(@NonNull SurfaceHolder holder) {
-                // Check camera permission
-                if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA)
-                        == PackageManager.PERMISSION_GRANTED) {
-                    startCamera();
-                } else {
-                    // Request camera permission
-                    ActivityCompat.requestPermissions(MainActivity.this,
-                            new String[]{Manifest.permission.CAMERA}, CAMERA_PERMISSION_REQUEST_CODE);
+                    switch (item.getItemId()) {
+                        case R.id.nav_qr:
+                            selectedFragment = new QRFragment();
+                            break;
+                        case R.id.nav_seat:
+                            selectedFragment = new SeatFragment();
+                            break;
+                    }
+
+                    if (selectedFragment != null) {
+                        getSupportFragmentManager().beginTransaction()
+                                .replace(R.id.fragmentContainer, selectedFragment)
+                                .commit();
+                        return true;
+                    } else {
+                        return false;
+                    }
                 }
-            }
-
-            @Override
-            public void surfaceChanged(@NonNull SurfaceHolder holder, int format, int width, int height) {
-            }
-
-            @Override
-            public void surfaceDestroyed(@NonNull SurfaceHolder holder) {
-                cameraSource.stop();
-            }
-        });
-
-        // Set barcode processor
-        barcodeDetector.setProcessor(new Detector.Processor<Barcode>() {
-            @Override
-            public void release() {
-            }
-
-            @Override
-            public void receiveDetections(Detector.Detections<Barcode> detections) {
-                final SparseArray<Barcode> barcodes = detections.getDetectedItems();
-                if (barcodes.size() != 0) {
-                    String qrCodeValue = barcodes.valueAt(0).displayValue;
-                    Log.d(TAG, "QR Code Value: " + qrCodeValue);
-                    // You can handle the scanned QR code value here
-                    // For example, display it in a TextView or pass it to another function
-                    showToast(qrCodeValue);
-                }
-            }
-        });
-    }
-
-    private void startCamera() {
-        try {
-            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-                return;
-            }
-            cameraSource.start(surfaceView.getHolder());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private void showToast(final String message) {
-        runOnUiThread(() -> Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show());
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        if (requestCode == CAMERA_PERMISSION_REQUEST_CODE) {
-            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                startCamera();
-            } else {
-                Toast.makeText(this, "Camera permission denied", Toast.LENGTH_SHORT).show();
-            }
-        } else {
-            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        }
-    }
+            };
 }
